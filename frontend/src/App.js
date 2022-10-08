5a0a6a5649700b072951590375266771a36c0162
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2022-10-08 20:34:22 +0530

    Lec 6 - done till 16:08 - Header and Fotter done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-const App = () => <h1 className="App">Hello World</h1>  
+const App = () => (
+  <>
+    <Header />
+    <main style={{minHeight: "93vh"}}></main>
+    <Footer />
+  </>
+);
 
 export default App;
