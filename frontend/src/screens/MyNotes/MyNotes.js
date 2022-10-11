afb8d1d29f8f673a88eae88da37a1ab91661308e
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2022-10-11 20:20:50 +0530

    finished lec 7 - done with MyNotes

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,42 +1,72 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
-import notes, {} from "../../data/notes";
+import notes from "../../data/notes";
 
 const MyNotes = () => {
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+
   return (
-    <MainScreen title="Welcome Back Vishal ..">
+    <MainScreen title="Welcome back Jay Mehta">
       <Link to="createnote">
-        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="1g">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
           Create New Note
         </Button>
-        {notes.map((note) => (
-          <Card style={{ margin: 10 }}>
-            <Card.Header style={{ display: "flex" }}>
-              <span
-                style={{
-                  color: "black",
-                  textDecoration: "none",
-                  flex: 1,
-                  cursor: "pointer",
-                  alignSelf: "center",
-                  fontSize: 18,
-                }}
-              >
-                {note.title}
-              </span>
-
-              <div>
-                <Button> Edit</Button>
-                <Button variant="danger" className="mx-2">
-                  Delete
-                </Button>
-              </div>
-            </Card.Header>
-          </Card>
-        ))}
       </Link>
+      {notes.map((note) => (
+        <Accordion defaultActiveKey={["0"]}>
+          <Accordion.Item eventkey="0">
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Button as={Card.Text} variant="link">
+                    {note.title}
+                  </Accordion.Button>
+                </span>
+                <div>
+                  <Button href={`/note/${note.id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={deleteHandler}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Collapse>
+                <Card.Body>
+                  <h4>
+                    <Badge bg="success" text="light">
+                      Category - {note.category}{" "}
+                    </Badge>
+                  </h4>
+
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Creater on - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
     </MainScreen>
   );
 };
