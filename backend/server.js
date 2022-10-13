3e6e698ca8a9c921494003d106cb208b5681ffc3
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2022-10-13 20:31:26 +0530

    lec 10 - till 9:57 , created route and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const notes = require("./data/notes");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes=require('./routes/userRoutes')
 
 const app = express();
 dotenv.config();
 
 connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running..");
@@ -16,11 +18,7 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  //   console.log(req.params);
-  res.send(note);
-});
+app.use("/api/users" , userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
