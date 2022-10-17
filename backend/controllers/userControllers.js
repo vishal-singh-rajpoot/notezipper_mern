3a542a727eb64b66503281f70886aa2ba69a37fc
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2022-10-17 17:08:55 +0530

    lec 10 - till 18:16 , create data into db and  get data from db

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,37 @@
-const registerUser = async (req , res) => {
-    const {name , email , password , pic} = req.body;
-    
-    res.json({
-        name ,
-        email,
-    });
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
-module.exports={ registerUser };
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    // user already registered
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } 
+  else {
+    res.status(400);
+    throw new Error("Error Occured !!");
+  }
+});
+
+module.exports = { registerUser };
---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      // just in case we need
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
