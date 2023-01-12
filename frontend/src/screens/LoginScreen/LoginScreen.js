30788bcd591f142d90124ec4dbb894290c532c3d
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-12 17:42:25 +0530

    lec 10 - till 45:09 , login functionality done , getting data on console from frontend

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,31 +1,80 @@
+import axios from "axios";
+import { useState } from "react";
 import { Button, Col , Form , Row } from "react-bootstrap";
 import { Link } from "react-router-dom"
 import MainScreen from "../../components/MainScreen";
 import "./LoginScreen.css";
 
 const LoginScreen = () => {
+    
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+    
+  const submitHandler =async (e) => {
+    e.preventDefault();
+    
+    try {
+        const config = {
+            headers: {
+                "Content-type":"application/json",
+            },
+        };
+        
+        setLoading(true);
+        
+        const { data } = await axios.post(
+            "/api/users/login",
+            {
+                email,
+                password,
+            },
+            // config
+        );
+        
+        console.log(data);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        
+        setLoading(false);
+    } catch (error) {
+        setError(error.response.data.message);
+    }
+  };
+  
+  
   return (
     <MainScreen title="LOGIN">
       <div className="loginContainer">
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Form.Group>
-          
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
         <Row className="py-3">
-            <Col>
+          <Col>
             New Customer ? <Link to="/register">Register Here</Link>
-            </Col>
+          </Col>
         </Row>
       </div>
     </MainScreen>
