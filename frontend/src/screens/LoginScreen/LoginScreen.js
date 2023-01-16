0c46224cc25ee9062025510be9822f31b6bba970
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-16 18:20:44 +0530

    till 21:28 , login action , calling login action in login screen

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,51 +1,64 @@
-import axios from "axios";
+// import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Col , Form , Row } from "react-bootstrap";
-import { Link } from "react-router-dom"
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ErrorMessage from "../../components/ErrorMessage";
 import Loading from "../../components/Loading";
 import MainScreen from "../../components/MainScreen";
 import "./LoginScreen.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../actions/userActions";
 
-const LoginScreen = ({history}) => {
-    
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-    
-  const submitHandler =async (e) => {
-    e.preventDefault();
-    
-    try {
-        const config = {
-            headers: {
-                "Content-type":"application/json",
-            },
-        };
-        
-        setLoading(true);
-        
-        const { data } = await axios.post(
-            "/api/users/login",
-            {
-                email,
-                password,
-            },
-            // config
-        );
-        
-        console.log(data);
-        localStorage.setItem("userInfo", JSON.stringify(data));
-        
-        setLoading(false);
-    } catch (error) {
-        setError(error.response.data.message);
-        setLoading(false);
+const LoginScreen = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+ 
+  useEffect(() => {
+    if(userInfo) {
+      history.push("/mynotes");
     }
+  }, [history, userInfo]);
+  
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    dispatch(login(email, password)); // call action
+
+    //   try {
+    //       const config = {
+    //           headers: {
+    //               "Content-type":"application/json",
+    //           },
+    //       };
+
+    //       setLoading(true);
+
+    //       const { data } = await axios.post(
+    //           "/api/users/login",
+    //           {
+    //               email,
+    //               password,
+    //           },
+    //           // config
+    //       );
+
+    //       console.log(data);
+    //       localStorage.setItem("userInfo", JSON.stringify(data));
+
+    //       setLoading(false);
+    //   } catch (error) {
+    //       setError(error.response.data.message);
+    //       setLoading(false);
+    //   }
   };
-  
-  
+
   return (
     <MainScreen title="LOGIN">
       <div className="loginContainer">
