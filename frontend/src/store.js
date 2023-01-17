1c87b4efd89560594420fcc79758235381c43a2e
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-17 20:29:40 +0530

    till 29:48 , reg reducer and action , get userInfo from localstorage - initial state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-    userLogin: userLoginReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = { 
+   userLogin: {userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
