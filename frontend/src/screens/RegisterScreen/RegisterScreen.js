3742dc4e6686a81c2924702ba0577092b5ee09d6
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-17 20:43:12 +0530

    11 finished - calling action in registration screen

--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import MainScreen from "../../components/MainScreen";
-import axios from "axios";
+// import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../../actions/userActions";
 
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
@@ -16,8 +18,21 @@ const RegisterScreen = () => {
   const [confirmpassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState(null);
   const [picMessage, setPicMessage] = useState(null);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const history = useHistory();
+  
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -25,46 +40,49 @@ const RegisterScreen = () => {
     if (password !== confirmpassword) {
       setMessage("Passwords do not match");
     } else {
-      setMessage(null);
-      try {
-        const config = {
-          headers: {
-            "Content-type": "application/json",
-          },
-        };
-        setLoading(true);
-
-        const { data } = await axios.post(
-          "/api/users",
-          {
-            name,
-            pic,
-            email,
-            password,
-          }
-          // config
-        );
-
-        console.log(data);
-        setLoading(false);
-        localStorage.setItem("userInfo", JSON.stringify(data));
-      } catch (error) {
-        setError(error.response.data.message);
-      }
+      dispatch(register(name, email, password, pic));
     }
+    // else {
+    //   setMessage(null);
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         "Content-type": "application/json",
+    //       },
+    //     };
+    //     setLoading(true);
+
+    //     const { data } = await axios.post(
+    //       "/api/users",
+    //       {
+    //         name,
+    //         pic,
+    //         email,
+    //         password,
+    //       }
+    //       // config
+    //     );
+
+    //     console.log(data);
+    //     setLoading(false);
+    //     localStorage.setItem("userInfo", JSON.stringify(data));
+    //   } catch (error) {
+    //     setError(error.response.data.message);
+    //   }
+    // }
   };
 
   const postDetails = (pics) => {
     if (!pics) {
       return setPicMessage("Please select an Image");
-    } 
+    }
     setPicMessage(null);
-    
-    if(pics.type==='image/jpeg' || pics.type === 'image/png') {
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
-      data.append('file',pics)
-      data.append('upload_preset','notezipper')
-      data.append('cloud_name','dude4arhz')
+      data.append("file", pics);
+      data.append("upload_preset", "notezipper");
+      data.append("cloud_name", "dude4arhz");
       fetch("https://api.cloudinary.com/v1_1/dude4arhz/image/upload", {
         method: "post",
         body: data,
@@ -81,7 +99,7 @@ const RegisterScreen = () => {
       return setPicMessage("Please Select an Image");
     }
   };
-  
+
   return (
     <MainScreen title="REGISTER">
       <div className="loginContainer">
@@ -138,7 +156,7 @@ const RegisterScreen = () => {
               onChange={(e) => postDetails(e.target.files[0])}
               id="custom-file"
               type="file"
-            //   label="Upload Profile Picture"
+              //   label="Upload Profile Picture"
               custom
             />
           </Form.Group>
