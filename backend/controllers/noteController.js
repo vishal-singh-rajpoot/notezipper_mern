751bccbc7eea70c6f841b1a585f2f5f4063d7ed9
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-18 20:46:35 +0530

    18:03 , POST - create a note API

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,9 +1,31 @@
 const Note = require("../models/noteModel");
 const asyncHandler = require("express-async-handler");
 
+// @desc    Get logged in user notes
+// @route   GET /api/notes
+// @access  Private
 const getNotes = asyncHandler(async (req, res) => {
-  const notes = await Note.find();
+  const notes = await Note.find({ user: req.user._id });
   res.json(notes);
 });
 
-module.exports = { getNotes };
+//@description     Create single Note
+//@route           GET /api/notes/create
+//@access          Private
+const createNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  if (!title || !content || !category) {
+    res.status(400);
+    throw new Error("Please Fill all the feilds");
+    return;
+  } else {
+    const note = new Note({ user: req.user._id, title, content, category });
+
+    const createdNote = await note.save();
+
+    res.status(201).json(createdNote);
+  }
+});
+
+module.exports = { getNotes, createNote };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { getNotes } = require("../controllers/noteController");
+const { getNotes, createNote } = require("../controllers/noteController");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").get(protect, getNotes);
-// router.route("/create").post();
+router.route("/create").post(protect, createNote);
 // router.route("/:id").get().delete().put();
 
 // router.get("/api/notes" , getNotes);
