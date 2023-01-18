57e32100540700e972f290d8fc80c446722963cb
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-18 19:46:56 +0530

    lec 12 - 4:30 , noteModel created and modified server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes=require('./routes/userRoutes');
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -21,6 +22,7 @@ app.get("/api/notes", (req, res) => {
 
 app.use("/api/users" , userRoutes);
 // app.use(userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
