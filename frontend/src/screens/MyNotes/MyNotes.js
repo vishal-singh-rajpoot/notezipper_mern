c91814e7c1c4baa4aee95c62c2ec2e35092b54e0
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-20 20:17:07 +0530

    lec 13 - till 13:34 , Reading notes redux - notesConstants , Reducer , store , Actions , screen

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,29 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Accordion, Badge, Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
-import axios from "axios";
+// import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import { listNotes } from "../../actions/notesActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const MyNotes = () => {
-  const [notes, setNotes] = useState([]);
-  
+  const dispatch = useDispatch();
+
+  const noteList = useSelector((state) => state.noteList);
+  const { loading, error, notes } = noteList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  // const [notes, setNotes] = useState([]);
+
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
     }
   };
 
-  const fetchNotes = async () => {
-    const {data} = await axios.get("/api/notes");
-    setNotes(data);
-    // console.log(notes);
-  };
-  
+  // const fetchNotes = async () => {
+  //   const { data } = await axios.get("/api/notes");
+  //   setNotes(data);
+  //   // console.log(notes);
+  // };
+
   console.log(notes);
 
+  const history = useHistory();
+  
   // React hook - fired up whenever a component is rendered , call api
   useEffect(() => {
-    fetchNotes();
-  }, []);
+    // fetchNotes();
+    dispatch(listNotes());
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [dispatch]);
 
   return (
     <MainScreen title="Welcome back Jay Mehta">
@@ -32,7 +51,9 @@ const MyNotes = () => {
           Create New Note
         </Button>
       </Link>
-      {notes.map((note) => (
+      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+      {loading && <Loading />}
+      {notes?.map((note) => (
         <Accordion key={note._id}>
           <Accordion.Item eventkey="0">
             <Card style={{ margin: 10 }}>
