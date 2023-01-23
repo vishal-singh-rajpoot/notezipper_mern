b04c9ac8c391d37ed20e0340c1b8ba43596b527d
RAJPOOT Vishal <VRT4@dsone.3ds.com>
2023-01-23 17:31:30 +0530

    15 lec done - deoployment server.js code

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes=require('./routes/userRoutes');
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+// const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -24,6 +25,37 @@ app.use("/api/users" , userRoutes);
 // app.use(userRoutes);
 app.use("/api/notes", noteRoutes);
 // app.use(noteRoutes);
+
+// --------------------------deployment------------------------------
+// const __dirname = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+// // --------------------------deployment------------------------------
+
+// // Error Handling middlewares
+// app.use(notFound);
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+//       .bold
+//   )
+// );
+
 
 app.use(notFound)
 app.use(errorHandler)
